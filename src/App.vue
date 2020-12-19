185bcf95536ec7bdbc435a386092bcb318c61653
Scott <scottqglobal@gmail.com>
2020-12-18 21:39:45 -0800

    basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hero></hero>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from "./components/Hero.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Hero,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'Lora', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 5rem 25rem 0 25rem;
+}
+
+.line-divider {
+  border: 0.25px solid rgba(0, 0, 0, 0.25);
+  width: 100%;
+  margin-left: 1rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faThumbtack)
+library.add(faBriefcase)
+library.add(faLightbulb)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
